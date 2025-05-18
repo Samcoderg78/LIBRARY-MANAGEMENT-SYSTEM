7abbbb7a2819733d39cb7456f21144bc20713ae3
SAURABH YADAV <sauravyadavg788@gmail.com>
2025-05-18 16:17:09 +0530

    Update app.py

--- Project folder/CODE/app.py
+++ Project folder/CODE/app.py
@@ -602,7 +602,7 @@ def stats_user():
 
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
